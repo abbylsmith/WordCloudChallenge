217b2d8b09fe6e6ba7224671cecce0fa682b7c6e
abbylsmith <als1@andrew.cmu.edu>
2016-09-28 17:13:47 -0400

    added test for t-distrib

--- test-this-2.R
+++ test-this-2.R
@@ -3,14 +3,17 @@
 
 library(testthat)
 
-test_that("length of output=n", {
+test_that("invalid first argument for length", {
   #note we did not need to set the seed here
-  expect_equal(length(rnorm(45, 0,2.5)), 45)
-  expect_equal(length(rnorm(0)), 0) #should not return error
-  expect_error(length(rnorm(-4))) #should return an error
+  expect_error(length(rnorm(-4))) #requires non-negative. should return an error
   expect_error(length(rnorm(NA))) #should return an error
   expect_warning(expect_error(length(rnorm("random characters")))) #shows an error AND warning
 })
+
+test_that("allows for there be a 0 length vectr", {
+  expect_equal(length(rnorm(0)), 0) 
+})
+
 
 test_that("mean of output= mean", {
   #when looking at the actual output we need to set the seed
@@ -23,10 +26,18 @@ test_that("mean of output= mean", {
   expect_warning(expect_equal(length(rnorm(30, NA)), 30)) #expect a warning but also recognizing that it will generate list of NAS, length=n
 })
 
-test_that("standard deviation of deviation of output=std", {
+test_that("standard deviation of output=std", {
   expect_equal({set.seed(500)
     sd(rnorm(200))}, 1, tol=.05)
   expect_warning({set.seed(500)
     rnorm(40,0, -4)}) #NAs produced when std deviation is below 0
   expect_warning(expect_equal(length(rnorm(30, 2, NA)), 30)) #expect a warning but also recognizing that it will generate list of NAS, length=n
 })
+
+test_that("t distribution does not meet tests", {
+  #shapiro test: if p value is less than alpha, we can reject the null hypothesis and conclude that 
+  #there is evidence that the data is NOT from a normally distributed population
+  expect_gt(shapiro.test(rnorm(30))$p.value, 0.05)
+  expect_lt(shapiro.test(rt(3000,10))$p.value, 0.05) #increasing degrees of freedom -> closer to normal
+})
+
